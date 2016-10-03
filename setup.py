34aa457b46cbcd7d59414c1386df54a6866eae9a
Stephan Meijer <me@stephanmeijer.com>
2016-10-03 12:17:05 +0200

    ??

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='select_random_filesystem',
-      version='1.0',
+      version='1.1',
       description='Select a random file or directory from the filesystem.',
       author='Stephan Meijer',
       author_email='me@stephanmeijer.com',
