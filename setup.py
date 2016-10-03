51c34dc05bae99f62bf415096db5cfefbb48470b
Stephan Meijer <me@stephanmeijer.com>
2016-10-03 12:12:46 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='select-random-filesystem',
+      version='1.0',
+      description='Select a random file or directory from the filesystem.',
+      author='Stephan Meijer',
+      author_email='me@stephanmeijer.com',
+      url='https://github.com/N-BodyPhysicsSimulator/select-random-filesystem'
+     )
